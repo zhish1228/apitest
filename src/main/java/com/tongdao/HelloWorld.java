b8951fdf249db1d2247326eac6a49e7667357a68
郑达 <2086@forex.com.cn>
2017-09-12 10:45:58 +0800

    add hello worl

---
+++ src/main/java/com/tongdao/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.tongdao;
+
+/**
+ * Created by zhengda on 2017/9/12.
+ */
+public class HelloWorld {
+
+    public static void main(String[] s){
+
+        System.out.println("hello world");
+
+    }
+
+}
