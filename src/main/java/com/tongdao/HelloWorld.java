60973e8262ea4afbcb4fef52de47cb0e1ec579cb
郑达 <2086@forex.com.cn>
2018-08-06 18:35:14 +0800

    add to

--- src/main/java/com/tongdao/HelloWorld.java
+++ src/main/java/com/tongdao/HelloWorld.java
@@ -6,6 +6,16 @@ package com.tongdao;
 public class HelloWorld {
 
   public static void main(String[] s) {
+
+    /**
+     * todo
+     * 1.db可控制用例是否执行
+     * 2.用例描述
+     * 3.方法自动加载
+     * 4.校验内容
+     * 5.记录response body
+     */
+
     System.out.println("hello world");
   }
 }
